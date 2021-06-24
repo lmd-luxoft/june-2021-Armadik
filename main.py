e88ad20604301ee71bb32e3c14ef346cd7c47b88
Рыжов Михаил Александрович (4040848) <maryzhov@vtb.ru>
2021-06-24 12:44:38 +0300

    del pass

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def main():
     """
     args = parse_args()
     args.func(args)
+    print(fs.read_file(args))
 
 
 if __name__ == '__main__':
--- server/FileService.py
+++ server/FileService.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 
 def create_dir(work_dir):
     """Создание рабочей директории"""
-    if os.path.exists(work_dir):
+    if not os.path.exists(work_dir):
         os.mkdir(work_dir)
 
 
@@ -13,7 +13,6 @@ def get_files(args):
     """
     work_dir = args.folder
     if os.path.exists(work_dir):
-        print('КАТАЛОГ:', work_dir)
         # Проверяем пустой ли каталог
         if not os.listdir(work_dir):
             print('Пустой')
@@ -29,8 +28,7 @@ def get_files(args):
 def get_file_data(args):
     """Get full info about file.
     """
-    data_file = str(args.folder + '/' + args.file_name)  # заменить на os.join
-    print('Проверяю файл:', data_file)
+    data_file = os.path.join(args.folder, args.file_name)
     try:
         with open(data_file, 'r') as fp:
             result = os.stat(data_file).st_mtime
@@ -44,25 +42,19 @@ def create_file(args):
     """Create a new file.
     """
     create_dir(args.folder)
-    data_file = str(args.folder + '/' + args.file_name)
-    print('Проверяю файл:', data_file)
-    try:
-        with open(data_file, 'r') as fp:
-            fp.readline()
-            print('Файл создан:', data_file)
-    except:
-        with open(data_file, 'w+')as fp:
-            print('Создание файла:', data_file)
+    data_file = os.path.join(args.folder, args.file_name)
+    create_dir(args.folder)
+    with open(data_file, 'w+')as fp:
+        print('Создание файла:', data_file)
 
 
 def read_file(args):
     """Read a new file.
     """
-    data_file = str(args.folder + '/' + args.file_name)
-    print('Проверяю файл:', data_file)
+    data_file = os.path.join(args.folder, args.file_name)
     try:
         with open(data_file, 'r') as fp:
-            print(fp.readline())
+            return fp.readline()
     except:
         print('Файл не найден')
 
@@ -70,7 +62,7 @@ def read_file(args):
 def delete_file(args):
     """Delete file.
     """
-    data_file = str(args.folder + '/' + args.file_name)
+    data_file = os.path.join(args.folder, args.file_name)
     print('Удаляю файл:', data_file)
     try:
         os.remove(data_file)
